e1226c60a7941ae0f2f6507f8e6392c7e7f6ba5c
Adeen Atif <73794279+adeen-atif@users.noreply.github.com>
2025-10-13 17:32:07 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
 import NFC from "./pages/NFC";
 import RealtimeAI from "./pages/RealtimeAI";
+import SpeakingPortfolio from "./pages/SpeakingPortfolio";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -24,6 +25,7 @@ const App = () => (
           <Route path="/blog/:slug" element={<BlogPost />} />
           <Route path="/NFC" element={<NFC />} />
           <Route path="/rt-ai" element={<RealtimeAI />} />
+          <Route path="/speakingportfolio" element={<SpeakingPortfolio />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
